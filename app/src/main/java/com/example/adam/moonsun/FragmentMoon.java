b353b124cfd39bba8f3ed6b5d244f8e529112c08
KhaledWielki <adam.rezner95@gmail.com>
2017-05-11 18:50:22 +0200

    Moon layout works

--- app/src/main/java/com/example/adam/moonsun/FragmentMoon.java
+++ app/src/main/java/com/example/adam/moonsun/FragmentMoon.java
@@ -6,26 +6,18 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import com.astrocalculator.AstroCalculator;
 import com.astrocalculator.AstroDateTime;
 
-import org.w3c.dom.Text;
-
+import java.text.DecimalFormat;
 import java.util.Date;
-import java.util.zip.Inflater;
 
-import static android.R.attr.timeZone;
-import static com.example.adam.moonsun.FragmentSettings.*;
 import static com.example.adam.moonsun.R.layout.fragment_moon;
-import static com.example.adam.moonsun.R.layout.fragment_settings;
 
 
 @RequiresApi(api = Build.VERSION_CODES.N)
@@ -35,17 +27,20 @@ public class FragmentMoon extends Fragment {
     public double longitude;
     public int refreshTime;
 
-    public double moonSetTime;
-    public double moonRiseTime;
-    public double closestNewMoon;
-    public double closestFullMoon;
-    public double moonFazePercents;
-    public int synodalMonthDay;
+    public String moonSetTime = "";
+    public String moonRiseTime = "";
+    public String closestNewMoon = "";
+    public String closestFullMoon = "";
+    public String moonFazePercents = "";
+    public String synodalMonthDay = "";
+
+    String month = "";
+    String day = "";
 
     TextView valueMoonSetTime;
     TextView valueMoonRiseTime;
     TextView valueClosestNewMoon;
-    TextView valueFullMoon;
+    TextView valueClosestFullMoon;
     TextView valueMoonFaze;
     TextView valueSynodalMonthDay;
 
@@ -54,38 +49,26 @@ public class FragmentMoon extends Fragment {
 
     }
 
-
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View myView = inflater.inflate(fragment_moon, container, false);
+
+        DecimalFormat decimalFormat = new DecimalFormat("#.##");
+
+        latitude = MainActivity.latitude;
+        longitude = MainActivity.longitude;
+        refreshTime = MainActivity.refreshTime;
+
         valueMoonSetTime = (TextView) myView.findViewById(R.id.value_moon_set_time);
         valueMoonRiseTime = (TextView) myView.findViewById(R.id.value_moon_rise_time);
         valueClosestNewMoon = (TextView) myView.findViewById(R.id.value_closets_new_moon);
-        valueFullMoon = (TextView) myView.findViewById(R.id.value_closets_full_moon);
+        valueClosestFullMoon = (TextView) myView.findViewById(R.id.value_closets_full_moon);
         valueMoonFaze = (TextView) myView.findViewById(R.id.value_moon_percentage_faze);
         valueSynodalMonthDay = (TextView) myView.findViewById(R.id.value_day_of_synodal_month);
 
-        latitude = MainActivity.latitude;
-        longitude = MainActivity.longitude;
-        refreshTime = MainActivity.refreshTime;
-//TODO tutaj skończyłem, trzeba dokończyć wyświetlanie layoutów
-        valueMoonSetTime.setText(Double.toString(moonSetTime));
-        valueMoonRiseTime.setText(Double.toString(moonRiseTime));
-        valueClosestNewMoon.setText(Integer.toString(closestNewMoon));
 
-        return myView;
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.N)
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-
-        latitude = MainActivity.latitude;
-        longitude = MainActivity.longitude;
-        refreshTime = MainActivity.refreshTime;
-
-        super.onCreate(savedInstanceState);
         int year = Calendar.getInstance().get(Calendar.YEAR);
         int month = Calendar.getInstance().get(Calendar.MONTH);
         month = month + 1;
@@ -103,9 +86,229 @@ public class FragmentMoon extends Fragment {
         AstroCalculator.Location astroLocation = new AstroCalculator.Location(latitude, longitude);
         AstroDateTime astroDateTime = new AstroDateTime(year, month, day, hour, minute, second, mGMTOffset, timeZone.inDaylightTime(new Date()));
         AstroCalculator astroCalculator = new AstroCalculator(astroDateTime, astroLocation);
-        AstroCalculator.SunInfo sunInfo = astroCalculator.getSunInfo();
+        AstroCalculator.MoonInfo moonInfo = astroCalculator.getMoonInfo();
 
-        
 
+        if(moonInfo.getMoonset().getHour() < 10 || moonInfo.getMoonset().getMinute() < 10) {
+            if(moonInfo.getMoonset().getHour() < 10 && moonInfo.getMoonset().getMinute() < 10) {
+                moonSetTime += "0"+moonInfo.getMoonset().getHour() + ":" + "0"+moonInfo.getMoonset().getMinute();
+            }
+            else if(moonInfo.getMoonset().getHour() < 10) {
+                moonSetTime += "0" + moonInfo.getMoonset().getHour() + ":" + moonInfo.getMoonset().getMinute();
+            }
+            else
+                moonSetTime += moonInfo.getMoonset().getHour() + ":" + "0" + moonInfo.getMoonset().getMinute();
+        }
+
+        if(moonInfo.getMoonrise().getHour() < 10 || moonInfo.getMoonrise().getMinute() < 10) {
+            if(moonInfo.getMoonrise().getHour() < 10 && moonInfo.getMoonrise().getMinute() < 10) {
+                moonRiseTime += "0"+moonInfo.getMoonrise().getHour() + ":" + "0"+moonInfo.getMoonrise().getMinute();
+            }
+            else if(moonInfo.getMoonrise().getHour() < 10) {
+                moonRiseTime += "0" + moonInfo.getMoonrise().getHour() + ":" + moonInfo.getMoonrise().getMinute();
+            }
+            else
+                moonRiseTime += moonInfo.getMoonrise().getHour() + ":" + "0" + moonInfo.getMoonrise().getMinute();
+        }
+
+        int temp = moonInfo.getNextFullMoon().getMonth();
+        switch (temp) {
+            case 1:
+            if(moonInfo.getNextFullMoon().getDay() < 10) {
+                closestFullMoon += "January " + "0" +moonInfo.getNextFullMoon().getDay();
+            } else
+                closestFullMoon += "January " +moonInfo.getNextFullMoon().getDay();
+                break;
+
+            case 2:
+                if(moonInfo.getNextFullMoon().getDay() < 10) {
+                    closestFullMoon += "February " + "0" +moonInfo.getNextFullMoon().getDay();
+                } else
+                    closestFullMoon += "February " +moonInfo.getNextFullMoon().getDay();
+                break;
+
+            case 3:
+                if(moonInfo.getNextFullMoon().getDay() < 10) {
+                    closestFullMoon += "March " + "0" +moonInfo.getNextFullMoon().getDay();
+                } else
+                    closestFullMoon += "March " +moonInfo.getNextFullMoon().getDay();
+                break;
+
+            case 4:
+                if(moonInfo.getNextFullMoon().getDay() < 10) {
+                    closestFullMoon += "April " + "0" +moonInfo.getNextFullMoon().getDay();
+                } else
+                    closestFullMoon += "April " +moonInfo.getNextFullMoon().getDay();
+                break;
+
+            case 5:
+                if(moonInfo.getNextFullMoon().getDay() < 10) {
+                    closestFullMoon += "May " + "0" +moonInfo.getNextFullMoon().getDay();
+                } else
+                    closestFullMoon += "May " +moonInfo.getNextFullMoon().getDay();
+                break;
+
+            case 6:
+                if(moonInfo.getNextFullMoon().getDay() < 10) {
+                    closestFullMoon += "June " + "0" +moonInfo.getNextFullMoon().getDay();
+                } else
+                    closestFullMoon += "June " +moonInfo.getNextFullMoon().getDay();
+                break;
+
+            case 7:
+                if(moonInfo.getNextFullMoon().getDay() < 10) {
+                    closestFullMoon += "July " + "0" +moonInfo.getNextFullMoon().getDay();
+                } else
+                    closestFullMoon += "July " +moonInfo.getNextFullMoon().getDay();
+                break;
+
+            case 8:
+                if(moonInfo.getNextFullMoon().getDay() < 10) {
+                    closestFullMoon += "August " + "0" +moonInfo.getNextFullMoon().getDay();
+                } else
+                    closestFullMoon += "August " +moonInfo.getNextFullMoon().getDay();
+                break;
+
+            case 9:
+                if(moonInfo.getNextFullMoon().getDay() < 10) {
+                    closestFullMoon += "September " + "0" +moonInfo.getNextFullMoon().getDay();
+                } else
+                    closestFullMoon += "September " +moonInfo.getNextFullMoon().getDay();
+                break;
+
+            case 10:
+                if(moonInfo.getNextFullMoon().getDay() < 10) {
+                    closestFullMoon += "October " + "0" +moonInfo.getNextFullMoon().getDay();
+                } else
+                    closestFullMoon += "October " +moonInfo.getNextFullMoon().getDay();
+                break;
+
+            case 11:
+                if(moonInfo.getNextFullMoon().getDay() < 10) {
+                    closestFullMoon += "November " + "0" +moonInfo.getNextFullMoon().getDay();
+                } else
+                    closestFullMoon += "November " +moonInfo.getNextFullMoon().getDay();
+                break;
+            case 12:
+                if(moonInfo.getNextFullMoon().getDay() < 10) {
+                    closestFullMoon += "December " + "0" +moonInfo.getNextFullMoon().getDay();
+                } else
+                    closestFullMoon += "December " +moonInfo.getNextFullMoon().getDay();
+                break;
+        }
+
+        int temp2 = moonInfo.getNextNewMoon().getMonth();
+        switch (temp2) {
+            case 1:
+                if(moonInfo.getNextNewMoon().getDay() < 10) {
+                    closestNewMoon += "January " + "0" +moonInfo.getNextNewMoon().getDay();
+                } else
+                    closestNewMoon += "January " +moonInfo.getNextNewMoon().getDay();
+                break;
+
+            case 2:
+                if(moonInfo.getNextNewMoon().getDay() < 10) {
+                    closestNewMoon += "February " + "0" +moonInfo.getNextNewMoon().getDay();
+                } else
+                    closestNewMoon += "February " +moonInfo.getNextNewMoon().getDay();
+                break;
+
+            case 3:
+                if(moonInfo.getNextNewMoon().getDay() < 10) {
+                    closestNewMoon += "March " + "0" +moonInfo.getNextNewMoon().getDay();
+                } else
+                    closestNewMoon += "March " +moonInfo.getNextNewMoon().getDay();
+                break;
+
+            case 4:
+                if(moonInfo.getNextNewMoon().getDay() < 10) {
+                    closestNewMoon += "April " + "0" +moonInfo.getNextNewMoon().getDay();
+                } else
+                    closestNewMoon += "April " +moonInfo.getNextNewMoon().getDay();
+                break;
+
+            case 5:
+                if(moonInfo.getNextNewMoon().getDay() < 10) {
+                    closestNewMoon += "May " + "0" +moonInfo.getNextNewMoon().getDay();
+                } else
+                    closestNewMoon += "May " +moonInfo.getNextNewMoon().getDay();
+                break;
+
+            case 6:
+                if(moonInfo.getNextNewMoon().getDay() < 10) {
+                    closestNewMoon += "June " + "0" +moonInfo.getNextNewMoon().getDay();
+                } else
+                    closestNewMoon += "June " +moonInfo.getNextNewMoon().getDay();
+                break;
+
+            case 7:
+                if(moonInfo.getNextNewMoon().getDay() < 10) {
+                    closestNewMoon += "July " + "0" +moonInfo.getNextNewMoon().getDay();
+                } else
+                    closestNewMoon += "July " +moonInfo.getNextNewMoon().getDay();
+                break;
+
+            case 8:
+                if(moonInfo.getNextNewMoon().getDay() < 10) {
+                    closestNewMoon += "August " + "0" +moonInfo.getNextNewMoon().getDay();
+                } else
+                    closestNewMoon += "August " +moonInfo.getNextNewMoon().getDay();
+                break;
+
+            case 9:
+                if(moonInfo.getNextNewMoon().getDay() < 10) {
+                    closestNewMoon += "September " + "0" +moonInfo.getNextNewMoon().getDay();
+                } else
+                    closestNewMoon += "September " +moonInfo.getNextNewMoon().getDay();
+                break;
+
+            case 10:
+                if(moonInfo.getNextNewMoon().getDay() < 10) {
+                    closestNewMoon += "October " + "0" +moonInfo.getNextNewMoon().getDay();
+                } else
+                    closestNewMoon += "October " +moonInfo.getNextNewMoon().getDay();
+                break;
+
+            case 11:
+                if(moonInfo.getNextNewMoon().getDay() < 10) {
+                    closestNewMoon += "November " + "0" +moonInfo.getNextNewMoon().getDay();
+                } else
+                    closestNewMoon += "November " +moonInfo.getNextNewMoon().getDay();
+                break;
+            case 12:
+                if(moonInfo.getNextNewMoon().getDay() < 10) {
+                    closestNewMoon += "December " + "0" +moonInfo.getNextNewMoon().getDay();
+                } else
+                    closestNewMoon += "December " +moonInfo.getNextNewMoon().getDay();
+                break;
+        }
+
+
+        moonFazePercents += decimalFormat.format((1 - moonInfo.getIllumination()) * 100) +"%";
+
+
+
+        valueMoonSetTime.setText(moonSetTime);
+        valueMoonRiseTime.setText(moonRiseTime);
+        valueClosestFullMoon.setText(closestFullMoon);
+        valueClosestNewMoon.setText(closestNewMoon);
+        valueMoonFaze.setText(moonFazePercents);
+
+
+
+        /*valueMoonSetTime.setText(Double.toString(moonSetTime));
+        valueMoonRiseTime.setText(Double.toString(moonRiseTime));
+        valueClosestNewMoon.setText(Double.toString(closestNewMoon));
+        valueClosestFullMoon.setText(Double.toString(closestFullMoon));
+        valueMoonFaze.setText(Double.toString(moonFazePercents));
+        valueSynodalMonthDay.setText(Integer.toString(synodalMonthDay));*/
+
+        return myView;
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
     }
 }
